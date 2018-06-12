b70e619ffb51af690df4e69672012607a8209f3f
Jason Whitlark <jason@whitlark.org>
2018-06-12 08:07:58 -0700

    Attempt to add JDK10 builds to the system.

--- update.sh
+++ update.sh
@@ -9,7 +9,9 @@ fi
 
 # config parameters
 
-OPENJDK_VERSION=8
+#OPENJDK_VERSION=${1:-8}
+
+declare -a JDK_VERSIONS=('8' '10')
 
 PAUL='Paul Lam <paul@quantisan.com>'
 WES='Wes Morgan <wesmorgan@icloud.com>'
@@ -39,29 +41,31 @@ EOH
 }
 
 for variant in "${variants[@]}"; do
-  openjdk_version=$OPENJDK_VERSION
-  dir="$variant"
-  bv=$(base_variant $variant)
-  bt=$(build_tool $variant)
-  bt_version=$(build_tool_version $bt)
-  echo "Generating Dockerfile for $dir"
-  [ -d "$dir" ] || continue
-  template="Dockerfile-$bt.template"
-  #echo "Using template $template"
-  if [ "$bv" != "debian" ]; then
-    openjdk_version="${openjdk_version}-${bv}"
-  fi
-  maintainer=${maintainers[$variant]:-${maintainers[$bv]}}
-  { generated_warning; cat "$template"; } > "$dir/Dockerfile"
-  ( sed -i.bak 's!%%BASE_TAG%%!'"$openjdk_version"'!g' "$dir/Dockerfile"
-    sed -i.bak 's!%%MAINTAINER%%!'"$maintainer"'!g' "$dir/Dockerfile"
-    sed -i.bak 's!%%BUILD_TOOL_VERSION%%!'"$bt_version"'!g' "$dir/Dockerfile"
-    if [ "$bv" = "alpine" ]; then
-      sed -i.bak 's/^%%ALPINE%% //g' "$dir/Dockerfile"
-    else
-        sed -i.bak '/^%%ALPINE%%/d' "$dir/Dockerfile"
-        sed -i.bak '/^$/N;/^\n$/D' "$dir/Dockerfile"
-    fi
-  )
-  find . -name "*.bak" -type f -delete
+    for OPENJDK_VERSION in "${JDK_VERSIONS[@]}"; do
+        openjdk_version=$OPENJDK_VERSION
+        dir="$variant"
+        bv=$(base_variant $variant)
+        bt=$(build_tool $variant)
+        bt_version=$(build_tool_version $bt)
+        echo "Generating Dockerfile for $dir"
+        [ -d "$dir" ] || continue
+        template="Dockerfile-$bt.template"
+        #echo "Using template $template"
+        if [ "$bv" != "debian" ]; then
+            openjdk_version="${openjdk_version}-${bv}"
+        fi
+        maintainer=${maintainers[$variant]:-${maintainers[$bv]}}
+        { generated_warning; cat "$template"; } > "$dir/Dockerfile-$openjdk_version"
+        ( sed -i.bak 's!%%BASE_TAG%%!'"$openjdk_version"'!g' "$dir/Dockerfile-$openjdk_version"
+          sed -i.bak 's!%%MAINTAINER%%!'"$maintainer"'!g' "$dir/Dockerfile-$openjdk_version"
+          sed -i.bak 's!%%BUILD_TOOL_VERSION%%!'"$bt_version"'!g' "$dir/Dockerfile-$openjdk_version"
+          if [ "$bv" = "alpine" ]; then
+              sed -i.bak 's/^%%ALPINE%% //g' "$dir/Dockerfile-$openjdk_version"
+          else
+              sed -i.bak '/^%%ALPINE%%/d' "$dir/Dockerfile-$openjdk_version"
+              sed -i.bak '/^$/N;/^\n$/D' "$dir/Dockerfile-$openjdk_version"
+          fi
+        )
+        find . -name "*.bak" -type f -delete
+  done
 done
