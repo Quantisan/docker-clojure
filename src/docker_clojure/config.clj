eff41bfc3c4250d6a9b684bf27b81325dffcc64a
Wes Morgan <git@wesmorgan.me>
2023-06-05 09:06:18 -0600

    Update tools-deps images to 1.11.1.1347

--- src/docker_clojure/config.clj
+++ src/docker_clojure/config.clj
@@ -70,7 +70,7 @@
 (def build-tools
   {"lein"       "2.10.0"
    "boot"       "2.8.3"
-   "tools-deps" "1.11.1.1323"})
+   "tools-deps" "1.11.1.1347"})
 
 (def default-build-tool "tools-deps")
 
@@ -78,8 +78,8 @@
   {"lein"       {"2.9.10" "dbb84d13d6df5b85bbf7f89a39daeed103133c24a4686d037fe6bd65e38e7f32"
                  "2.10.0" "b1757ce941e4cbf15cbf649b7b4f413365e612da892d22841ec1728391bb66af"}
    "boot"       {"2.8.3" "0ccd697f2027e7e1cd3be3d62721057cbc841585740d0aaa9fbb485d7b1f17c3"}
-   "tools-deps" {"1.11.1.1273" "4938afe6c0690d2a6553141857742d94a7350c02b4fa57cd6a9c1b7cbe66492e"
-                 "1.11.1.1323" "c4666f0e2c2397b91554befd71ff6149b2e89acbf90400e1dcf557526cfb593d"}})
+   "tools-deps" {"1.11.1.1323" "c4666f0e2c2397b91554befd71ff6149b2e89acbf90400e1dcf557526cfb593d"
+                 "1.11.1.1347" "d9158bf3a1d92fbf8551656e47a86f42e93d10f1db9defa2124bfee206ce8c8f"}})
 
 (def exclusions ; don't build these for whatever reason(s)
   #{; boot on JDK 8 & Alpine is encountering a TLS handshake error trying to
