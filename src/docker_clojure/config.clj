068b549332de7affde2fda7424844654a95ed460
Wes Morgan <git@wesmorgan.me>
2025-09-22 15:48:20 -0600

    Add JDK 25 to build matrix

--- src/docker_clojure/config.clj
+++ src/docker_clojure/config.clj
@@ -73,7 +73,7 @@
 
 (def git-repo "https://github.com/Quantisan/docker-clojure.git")
 
-(def jdk-versions #{8 11 17 21 24})
+(def jdk-versions #{8 11 17 21 24 25})
 
 (def base-images
   "Map of JDK version to base image name(s) with :default as a fallback"
