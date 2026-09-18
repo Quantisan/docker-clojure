8c81a6d5c29c308ef9c92c12cbe272bbaf1699bb
Wes Morgan <git@wesmorgan.me>
2026-09-18 14:49:07 -0600

    Clean up some vestigial specs

--- src/docker_clojure/config.clj
+++ src/docker_clojure/config.clj
@@ -20,10 +20,8 @@
     ;; Generate in-range by construction; the default pos-int generator yields
     ;; values < 8 the >= 8 such-that can't satisfy at small sizes (flaky gen).
     #(gen/choose 8 30)))
-(s/def ::jdk-versions (s/coll-of ::jdk-version :distinct true :into #{}))
 
 (s/def ::base-image ::non-blank-string)
-(s/def ::base-images (s/coll-of ::base-image :distinct true :into #{}))
 
 (def docker-image-name-re (re-pattern "[-\\w]+(?::[-\\w.]+)?"))
 
@@ -56,8 +54,6 @@
     #(gen/fmap (fn [[namespace local]] (keyword namespace local))
                (gen/vector (gen'/string-from-regex distro-component-re) 2))))
 
-(s/def ::distros (s/coll-of ::distro :distinct true :into #{}))
-
 (def specific-build-tools #{"lein" "tools-deps"})
 (s/def ::specific-build-tool specific-build-tools)
 (s/def ::build-tool (s/or ::specific-tool ::specific-build-tool
@@ -82,8 +78,6 @@
                (gen/vector (s/gen ::specific-build-tool-version)
                            (count specific-build-tools)))))
 
-(s/def ::maintainers
-  (s/coll-of ::non-blank-string :distinct true :into #{}))
 (s/def ::maintainer ::non-blank-string)
 
 (s/def ::architecture ::non-blank-string)
